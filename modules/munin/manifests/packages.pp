0aecca64a805dadf06cf93263973fc63b32bd90c
Simon <me@simonhollingshead.com>
2016-02-07 20:51:58 +0000

    Fix munin notify.

--- modules/munin/manifests/packages.pp
+++ modules/munin/manifests/packages.pp
@@ -4,13 +4,14 @@ class munin::packages {
         owner  => root,
         group  => root,
         source => "puppet:///modules/munin/karma.py",
-        require => [Package["munin"],Package["praw3"]]
+        require => [Package["munin"],Package["praw3"]],
+	notify => Exec["reload-munin"] # Reload when file changes.
     }
     
     file { "/etc/munin/plugins/karma":
         ensure => link,
         target => "/usr/share/munin/plugins/karma.py",
         require => File["/usr/share/munin/plugins/karma.py"],
-        notify => Exec["reload-munin"]
+        notify => Exec["reload-munin"] # Reload when initially installed.
     }
 }
