ca97e6ddcd3f5eabdd60c07cff4a7e21ac90c01a
Simon <me@simonhollingshead.com>
2016-02-09 20:05:28 +0000

    Merge karma scores so axis reads better.

--- modules/munin/files/karma.py
+++ modules/munin/files/karma.py
@@ -5,9 +5,7 @@ if len(sys.argv) >= 2 and sys.argv[1] == "config":
 	print("graph_category Web")
 	print("graph_title Reddit Karma")
 	print("graph_vlabel Karma")
-	print("link_karma.label Link Karma")
-	print("comment_karma.label Comment Karma")
+	print("karma.label Karma")
 else:
 	elpasi = praw.Reddit("/u/elpasi Munin grapher").get_redditor("elpasi")
-	print("link_karma.value {:d}".format(elpasi.link_karma))
-	print("comment_karma.value {:d}".format(elpasi.comment_karma))
+	print("karma.value {:d}".format((elpasi.link_karma+elpasi.comment_karma)))
