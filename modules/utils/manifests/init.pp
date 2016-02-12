66b479f4c6e2515dff7d801b245f1a9c88ca24d4
Simon <me@simonhollingshead.com>
2016-02-12 21:33:52 +0000

    Remove packages from koding to save HDD space.

--- modules/utils/manifests/init.pp
+++ modules/utils/manifests/init.pp
@@ -2,4 +2,9 @@ class utils {
 	package { ["vim","lynx","elinks","git","gitk","mtr","nmon","htop","nethogs","patch","rsync"]:
 		ensure => present
 	}
+	
+	# TODO: Remove when no longer using Koding.
+	package { ["golang-go","golang-go-linux-amd64","puppetserver"]:
+	    ensure => purged
+	}
 }
