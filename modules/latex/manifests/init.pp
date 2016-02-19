7f9757288907baff762b86f99a04dd75284f281c
Simon Hollingshead <me@simonhollingshead.com>
2016-02-19 03:02:23 +0000

    Add all files required for CV to render.

--- modules/latex/manifests/init.pp
+++ modules/latex/manifests/init.pp
@@ -1,5 +1,16 @@
 class latex {
-    package { ["texlive-xetex","latex-xcolor","texlive-math-extra"]:
+    package { ["texlive-xetex","latex-xcolor","texlive-math-extra","texlive-latex-extra","texlive-bibtex-extra"]:
         ensure => present
     }
+	
+	file { "/usr/share/fonts/helveticaneue":
+		ensure => directory,
+		recurse => true,
+		source => "puppet:///modules/latex/helveticaneue",
+		purge => true,
+		owner => root,
+		group => root,
+		mode => "0644",
+		ignore => "*.gpg"
+	}
 }
