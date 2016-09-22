6cfde5943385ec21ababc420611756a16df0b89d
Simon Hollingshead <me@simonhollingshead.com>
2016-09-22 23:03:45 +0000

    Begin work on parser.

--- modules/domainwatch/files/listener.rb
+++ modules/domainwatch/files/listener.rb
@@ -1,13 +1,21 @@
 #!/usr/bin/ruby
 
 require "bunny"
+require "whois"
 conn = Bunny.new
 conn.start
 ch = conn.create_channel
 q = ch.queue("domains", :durable => true, :auto_delete => false)
 
+whois_props = Whois::Record::Parser::PROPERTIES
+
 q.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
   puts "Received '#{body}'"
-  sleep 1.0
+  # TODO: Check the domain is valid, and is not already looked up recently.
+  record = Whois.whois(body)
+  whois_props.each{ |property|
+    puts "Property: %{property} is %{supported}: %{response}" % {:property=>property, :supported=>record.property_any_supported?(property)?"Supported":"Unsupported", :response=>record.send(property)}
+    # TODO: Contact, Registrar and Nameserver are special and need to be further unpacked.  When placed into ElasticSearch, these should be nested fields instead.
+  }
   ch.ack(delivery_info.delivery_tag)
 end
