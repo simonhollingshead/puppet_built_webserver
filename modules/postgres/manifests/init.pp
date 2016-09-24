49cb117ea51acdc8ee07fbba42c0dc66d72fe774
Simon Hollingshead <me@simonhollingshead.com>
2016-09-24 19:56:40 +0000

    Put flyway into a predictably named directory.

--- modules/postgres/manifests/init.pp
+++ modules/postgres/manifests/init.pp
@@ -4,15 +4,23 @@ class postgres ($db_admin_password) {
 	}
 	
 	wget::fetch { "Flyway":
-		source => "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/4.0/flyway-commandline-4.0.tar.gz",
+		source => "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/4.0.3/flyway-commandline-4.0.3.tar.gz",
 		destination => "/opt/flyway.tar.gz",
 		cache_dir => "/var/cache/wget",
 	}
+	
+	file { "/opt/flyway":
+		ensure => directory,
+		mode => "0755",
+		owner => root,
+		group => root
+	}
 		
 	exec { "Extract Flyway":
-		command => "/bin/tar -C /opt/ -xf /opt/flyway.tar.gz",
+		command => "/bin/tar -C /opt/flyway -xf /opt/flyway.tar.gz --strip-components 1",
 		refreshonly => true,
-		subscribe => File["/opt/flyway.tar.gz"]
+		subscribe => File["/opt/flyway.tar.gz"],
+		require => File["/opt/flyway"]
 	}
 	
 	file { "/sql":
