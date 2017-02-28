5259693ea0e46dd58b1506ff63de08b5c38be121
Simon Hollingshead <me@simonhollingshead.com>
2017-02-28 22:21:52 +0000

    Remove elasticsearch.

--- modules/second/manifests/init.pp
+++ modules/second/manifests/init.pp
@@ -1,10 +1,10 @@
 class second {
-	class { 'elasticsearch':
-	    java_install => true,
-            manage_repo  => true,
-            repo_version => '2.x',
-	    config => { 'cluster.name' => 'domainwatch' }
-	}
+	#class { 'elasticsearch':
+	#    java_install => true,
+        #    manage_repo  => true,
+        #    repo_version => '2.x',
+	#    config => { 'cluster.name' => 'domainwatch' }
+	#}
 	
 	class { 'unattended_upgrades': 
 		random_sleep => 100
